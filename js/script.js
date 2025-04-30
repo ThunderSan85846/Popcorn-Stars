ba2c794fac92e4d0c7f24cbdd66572f6ce534f08
ThunderSan85846 <gserrato3113@gmail.com>
2025-04-30 20:49:10 -0300

    javascript

--- js/script.js
+++ js/script.js
@@ -1,17 +1,46 @@
 let users = {};
+const clickSound = document.getElementById("click-sound");
+
+function playSound() {
+  clickSound.currentTime = 0;
+  clickSound.play();
+}
+
+function typeWriter(text, elementId, callback) {
+  const element = document.getElementById(elementId);
+  element.innerHTML = "";
+  let i = 0;
+  const interval = setInterval(() => {
+    element.innerHTML += text.charAt(i);
+    i++;
+    if (i === text.length) {
+      clearInterval(interval);
+      if (callback) callback();
+    }
+  }, 40);
+}
+
+window.onload = () => {
+  const welcomeText = "Olá, bem-vindo ao Popcorn Stars!\nVocê parece ser novo por aqui";
+  typeWriter(welcomeText, "welcome-text", () => {
+    document.getElementById("tutorial-buttons").classList.remove("hidden");
+  });
+};
 
 function startTutorial() {
+  playSound();
   alert("Iniciando o tutorial...");
-  showScreen("intro");
+  showScreen("account");
 }
 
 function skipTutorial() {
+  playSound();
   alert("Pulando o tutorial. Indo direto para o jogo!");
-  showScreen("intro");
+  showScreen("account");
 }
 
 function showScreen(screen) {
-  // Oculta todas as seções
+  playSound();
   document.querySelectorAll(".modal").forEach(div => div.classList.add("hidden"));
   document.getElementById("game-screen").style.display = "none";
 
@@ -23,6 +52,7 @@ function showScreen(screen) {
 }
 
 function handleLogin() {
+  playSound();
   const username = document.getElementById("login-username").value;
   const password = document.getElementById("login-password").value;
 
@@ -35,6 +65,7 @@ function handleLogin() {
 }
 
 function handleRegister() {
+  playSound();
   const username = document.getElementById("register-username").value;
   const password = document.getElementById("register-password").value;
 
